c81796c58fd0829789ea2534a3bd0d5cd6db3e29
John Griffiths <j.davidgriffiths@gmail.com>
2018-04-15 13:01:36 -0400

    added output filenames

---
+++ scratch/jg_record_generate_SSVEP.bat
@@ -0,0 +1,8 @@
+
+
+start python C:\Users\John\GitBash\muse-lsl\stimulus_presentation\generate_SSVEP.py -d 10
+
+start python C:\Users\John\GitBash\muse-lsl\lsl-record.py -d 10 -f "jg_record_generate_SSVEP_bat_test_data.csv"
+
+
+
