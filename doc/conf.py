1f8ea26af8d6d94646d9bb060fd41eda9759f10d
John Griffiths <JohnGriffiths@users.noreply.github.com>
2018-05-13 14:12:36 -0400

    Create conf.py

---
+++ doc/conf.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Use sphinx-quickstart to create your own conf.py file!
+# After that, you have to edit a few things.  See below.
+
+# Select nbsphinx and, if needed, add a math extension (mathjax or pngmath):
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+]
+
+# Exclude build directory and Jupyter backup files:
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+# Default language for syntax highlighting in reST and Markdown cells
+highlight_language = 'none'
+
+# Don't add .txt suffix to source files (available for Sphinx >= 1.5):
+html_sourcelink_suffix = ''
+
+# Execute notebooks before conversion: 'always', 'never', 'auto' (default)
+#nbsphinx_execute = 'never'
+
+# Use this kernel instead of the one stored in the notebook metadata:
+#nbsphinx_kernel_name = 'python3'
+
+# List of arguments to be passed to the kernel that executes the notebooks:
+#nbsphinx_execute_arguments = ['--InlineBackend.figure_formats={"png", "pdf"}']
+
+# If True, the build process is continued even if an exception occurs:
+#nbsphinx_allow_errors = True
+
+# Controls when a cell will time out (defaults to 30; use -1 for no timeout):
+#nbsphinx_timeout = 60
+
+# Default Pygments lexer for syntax highlighting in code cells:
+#nbsphinx_codecell_lexer = 'ipython3'
+
+# Width of input/output prompts used in CSS:
+#nbsphinx_prompt_width = '8ex'
+
+# If window is narrower than this, input/output prompts are on separate lines:
+#nbsphinx_responsive_width = '700px'
+
+# This is processed by Jinja2 and inserted before each notebook
+nbsphinx_prolog = r"""
+{% set docname = env.doc2path(env.docname, base='doc') %}
+
+.. only:: html
+
+    .. role:: raw-html(raw)
+        :format: html
+
+    .. nbinfo::
+
+        This page was generated from `{{ docname }}`__.
+        Interactive online version:
+        :raw-html:`<a href="https://mybinder.org/v2/gh/spatialaudio/nbsphinx/{{ env.config.release }}?filepath={{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge.svg" style="vertical-align:text-bottom"></a>`
+
+    __ https://github.com/spatialaudio/nbsphinx/blob/
+        {{ env.config.release }}/{{ docname }}
+
+.. raw:: latex
+
+    \vfil\penalty-1\vfilneg
+    \vspace{\baselineskip}
+    \textcolor{gray}{The following section was generated from
+    \texttt{\strut{}{{ docname }}}\\[-0.5\baselineskip]
+    \noindent\rule{\textwidth}{0.4pt}}
+    \vspace{-2\baselineskip}
+"""
+
+# This is processed by Jinja2 and inserted after each notebook
+nbsphinx_epilog = r"""
+.. raw:: latex
+
+    \textcolor{gray}{\noindent\rule{\textwidth}{0.4pt}\\
+    \hbox{}\hfill End of
+    \texttt{\strut{}{{ env.doc2path(env.docname, base='doc') }}}}
+    \vfil\penalty-1\vfilneg
+"""
+
+# -- The settings below this line are not specific to nbsphinx ------------
+
+master_doc = 'index'
+
+project = 'eeg-notebooks'
+author = 'John Griffiths, Dano Morrison, et al.'
+copyright = '2018, ' + author
+
+linkcheck_ignore = [r'http://localhost:\d+/']
+
+# -- Get version information from Git -------------------------------------
+
+try:
+    from subprocess import check_output
+    release = check_output(['git', 'describe', '--tags', '--always'])
+    release = release.decode().strip()
+except Exception:
+    release = '<unknown>'
+
+# -- Options for HTML output ----------------------------------------------
+
+html_title = project + ' version ' + release
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'collapse_navigation': False,
+}
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+    'papersize': 'a4paper',
+    'printindex': '',
+    'preamble': r"""
+\usepackage[sc,osf]{mathpazo}
+\linespread{1.05}  % see http://www.tug.dk/FontCatalogue/urwpalladio/
+\renewcommand{\sfdefault}{pplj}  % Palatino instead of sans serif
+\IfFileExists{zlmtt.sty}{
+    \usepackage[light,scaled=1.05]{zlmtt}  % light typewriter font from lmodern
+}{
+    \renewcommand{\ttdefault}{lmtt}  % typewriter font from lmodern
+}
+""",
+}
+
+latex_documents = [
+    (master_doc, 'nbsphinx.tex', project, author, 'howto'),
+]
+
+latex_show_urls = 'footnote'
