ad2a90c4481b1c3c8ba6b39dbebbc56f52f0a6c9
John Griffiths <j.davidgriffiths@gmail.com>
2018-06-03 15:37:57 -0400

    switched to flagged argumnent parsing and added eeg timestamp conversion option

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -2,8 +2,12 @@
 
 import sys
 import subprocess
+from optparse import OptionParser
 
-def run_experiment(expt_name,subj_num = '',sess_num = '',muse_lsl_dir = 'muse-lsl'):
+
+
+def run_experiment(expt_name,subj_num = '',sess_num = '',muse_lsl_dir = 'muse-lsl',
+		   convert_eeg_timestamps_to_localclock = '0'):
 
 
     lsl_record_file = '%s/lsl-record.py' % muse_lsl_dir
@@ -12,13 +16,16 @@ def run_experiment(expt_name,subj_num = '',sess_num = '',muse_lsl_dir = 'muse-ls
         expt_script_filename = expt_name.replace('mlsl_', 'generate_').replace('_test', '') + '.py'
         expt_script_file = '%s/stimulus_presentation/%s' %(muse_lsl_dir, expt_script_filename)
 
+
+    expt_script_args = ''
+    lsl_record_args = ''
     if 'test' in expt_name: 
-        expt_script_args = ' -d 20'
-	lsl_record_args = ' -d 20'
-    else:
-        expt_script_args = ''
-        lsl_record_args = ''
-	
+        expt_script_args += ' -d 20'
+	lsl_record_args += ' -d 20'
+
+    lsl_record_args += ' -c ' + convert_eeg_timestamps_to_localclock
+
+
     expt_script_call = '%s %s' %(expt_script_file, expt_script_args)
     lsl_record_call = '%s %s' %(lsl_record_file, lsl_record_args)
 
@@ -41,7 +48,14 @@ if __name__ == '__main__':
     """
     Usage:
 
-    python run_eeg_experiment.py EXPT_NAME SUBJECT_NUM SESS_NUM 
+    python run_experiment.py [ARGS]
+    
+    Options: 
+    
+       -e EXPT_NAME 
+       -c CONVERT_TIMESTAMPS 
+       --subj SUBJECT_NUM 
+       --sess SESS_NUM 
 
 
     Experiment names:
@@ -57,27 +71,49 @@ if __name__ == '__main__':
     Add the '_test' to run a quick (20s) version
 
 
+    The CONVERT_TIMESTAMPS argument says whether or not to apply an internal conversion to the 
+    timestamps coming from the EEG device. This should be set to '1' if using BlueMuse <= 1.0.5
+
+
+
     Examples:
 
-    python run_experiment.py mlsl_N170_test 
+    python run_experiment.py -e mlsl_N170_test -c 1 
 
  
-    python run_experiment.py mlsl_SSVEP
+    python run_experiment.py -e mlsl_SSVEP -c 0
 
 
 
 
     """
 
-    expt_name = sys.argv[1]
+    parser = OptionParser()
+
+    parser.add_option("-e", "--expt",
+                      dest="expt_name", type='str', default=None,
+		      help="experiment name")
+
+    parser.add_option("--subj", "--subj_num",
+                      dest="subj_num", type='str', default='',
+    		      help="subject num")
+
+    parser.add_option("--sess", "--sess_num",
+                      dest="sess_num", type='str', default='',
+                      help="session num")
+
+    parser.add_option("-c", "--convert_timestamp",
+                      dest="c", type='str', default='0',
+		      help="convert eeg timestamp to pylsl local clock")
+
+    (options, args) = parser.parse_args()
+
+
+
+
+    run_experiment(options.expt_name,
+                   options.subj_num,
+		   options.sess_num, 
+                   convert_eeg_timestamps_to_localclock=options.c)
     
-    if len(sys.argv) == 4:
-       subj_num,sess_num = sys.argv[2:4]
-    else:
-       subj_num = ''
-       sess_num = ''
-
-    run_experiment(expt_name,subj_num,sess_num)
-
-
 
