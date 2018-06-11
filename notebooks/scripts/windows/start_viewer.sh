2ab66714884cec832ebf530ddacdf36f040ff39d
jdpigeon <morrisondano@gmail.com>
2018-06-11 17:33:28 -0400

    Added run_experiment.py back for backwards compatibility

---
+++ notebooks/scripts/windows/start_viewer.sh
@@ -0,0 +1,9 @@
+
+PYTHON_EXE=$HOME/Miniconda2/python.exe
+EEG_NB_DIR=$HOME/GitBash/eeg-notebooks
+MUSE_LSL_DIR=$EEG_NB_DIR/muse-lsl
+
+#start winpty $PYTHON_EXE $EEG_NB_DIR/muse-lsl.py
+
+start winpty $PYTHON_EXE $MUSE_LSL_DIR/lsl-viewer.py
+
