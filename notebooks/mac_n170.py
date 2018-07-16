61452d3f66c9fba0983950b438faee63d2f2eb19
amandakeasson <easson.amanda@gmail.com>
2018-07-16 15:52:56 -0400

    changed save path

--- notebooks/mac_n170.py
+++ notebooks/mac_n170.py
@@ -10,15 +10,34 @@ from collections import OrderedDict
 import warnings
 warnings.filterwarnings('ignore')
 
+
+from optparse import OptionParser
+parser = OptionParser()
+ 
+parser.add_option("-d", "--duration",
+                  dest="duration", type='int', default=400,
+                  help="duration of the recording in seconds")
+parser.add_option("-s", "--subject",
+                  dest="subject", type='int', default=1,
+                  help="subject number: must be an integer")
+parser.add_option("-r", "--run",
+                  dest="run", type='int', default=1,
+                  help="run (session) number: must be an integer")
+parser.add_option("-p", "--path",
+                  dest="path", type='string', default="~/eeg-notebooks/notebooks/test.csv",
+                  help="path to save recording")
+
+
+
+
 #import pygame
 #pygame.init()
 import sys
 
-recording_path = '~/codeOther/eeg-notebooks/notebooks/test.csv'
-
-duration = 30
-subject = 1
+duration = 120
+subject = 2
 session = 1
+recording_path = '~/eeg-notebooks/notebooks/test1.csv'
 
 stimulus = Process(target=n170.present, args=(duration,))
 recording = Process(target=record, args=(duration, recording_path))
