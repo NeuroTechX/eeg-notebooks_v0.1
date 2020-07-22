12d6e1100e0db64703c125bc206bbddcaff7cf6c
John Griffiths <j.davidgriffiths@gmail.com>
2020-07-22 16:43:49 -0400

    update from bfnb

--- eegnb/analysis/utils.py
+++ eegnb/analysis/utils.py
@@ -1,130 +1,101 @@
-# -*- coding: utf-8 -*-
-
 from glob import glob
 import os
 from collections import OrderedDict
 
 from mne import create_info, concatenate_raws
 from mne.io import RawArray
-from mne.channels import read_montage
+from mne.channels import make_standard_montage
 import pandas as pd
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
+
+from eegnb import DATA_DIR
+from eegnb.devices.utils import CHANNEL_INDICES, STIM_INDICES, SAMPLE_FREQS
 
 
 sns.set_context('talk')
 sns.set_style('white')
 
 
-def load_muse_csv_as_raw(filename, sfreq=256., ch_ind=[0, 1, 2, 3],
-                         stim_ind=5, replace_ch_names=None, verbose=1):
-    """Load CSV files into a Raw object.
-
-    Args:
-        filename (str or list): path or paths to CSV files to load
-
-    Keyword Args:
-        subject_nb (int or str): subject number. If 'all', load all
-            subjects.
-        session_nb (int or str): session number. If 'all', load all
-            sessions.
-        sfreq (float): EEG sampling frequency
-        ch_ind (list): indices of the EEG channels to keep
-        stim_ind (int): index of the stim channel
-        replace_ch_names (dict or None): dictionary containing a mapping to
-            rename channels. Useful when an external electrode was used.
-
-    Returns:
-        (mne.io.array.array.RawArray): loaded EEG
-    """
+def load_csv_as_raw(filename, sfreq, ch_ind, stim_ind,
+                              replace_ch_names=None, verbose=1):
+    """"""
     n_channel = len(ch_ind)
-
     raw = []
-    for fname in filename:
-        # read the file
-        data = pd.read_csv(fname, index_col=0)
+    for fn in filename:
+        # Read the file
+        data = pd.read_csv(fn, index_col=0)
 
-        # name of each channels
-        ch_names = list(data.columns)[0:n_channel] + ['Stim']
+        # Channel names and types
+        ch_names = list(data.columns)[0:n_channel] + ['stim']
+        print(ch_names)
+        ch_types = ['eeg'] * n_channel + ['stim']
 
         if replace_ch_names is not None:
             ch_names = [c if c not in replace_ch_names.keys()
                         else replace_ch_names[c] for c in ch_names]
 
-        # type of each channels
-        ch_types = ['eeg'] * n_channel + ['stim']
-        montage = read_montage('standard_1005')
-
-        # get data and exclude Aux channel
+        # Transpose EEG data and convert from uV to Volts
         data = data.values[:, ch_ind + [stim_ind]].T
-
-        # convert in Volts (from uVolts)
         data[:-1] *= 1e-6
 
         # create MNE object
-        info = create_info(ch_names=ch_names, ch_types=ch_types,
-                           sfreq=sfreq, montage=montage, verbose=verbose)
-        raw.append(RawArray(data=data, info=info, verbose=verbose))
+        info = create_info(ch_names=ch_names, ch_types=ch_types, sfreq=sfreq,verbose=1)
+        raw.append(RawArray(data=data, info=info, verbose=1))
 
-    # concatenate all raw objects
     raws = concatenate_raws(raw, verbose=verbose)
+    montage = make_standard_montage('standard_1005')
+    raws.set_montage(montage)
 
     return raws
 
 
-def load_data(data_dir=None, subject_nb=1, session_nb=1, sfreq=256.,
-              ch_ind=[0, 1, 2, 3], stim_ind=5, replace_ch_names=None, verbose=1):
+def load_data(subject_id, session_nb, board_name, experiment, replace_ch_names=None, verbose=1):
     """Load CSV files from the /data directory into a Raw object.
-
     Args:
         data_dir (str): directory inside /data that contains the
             CSV files to load, e.g., 'auditory/P300'
-
     Keyword Args:
-        subject_nb (int or str): subject number. If 'all', load all
+        subject_id (int or str): subject number. If 'all', load all
             subjects.
         session_nb (int or str): session number. If 'all', load all
             sessions.
-        sfreq (float): EEG sampling frequency
-        ch_ind (list): indices of the EEG channels to keep
-        stim_ind (int): index of the stim channel
         replace_ch_names (dict or None): dictionary containing a mapping to
             rename channels. Useful when an external electrode was used.
-
     Returns:
         (mne.io.array.array.RawArray): loaded EEG
     """
-    if subject_nb == 'all':
-        subject_nb = '*'
+
+
+    if subject_id == 'all':
+        subject_id = '*'
     if session_nb == 'all':
         session_nb = '*'
 
-    data_path = os.path.join(data_dir,
-                             'subject{}/session{}/*.csv'.format(subject_nb, session_nb))
+    data_path = os.path.join(DATA_DIR, experiment, subject_id, f'{board_name}_TRIAL_{session_nb}.csv')
     fnames = glob(data_path)
 
-    return load_muse_csv_as_raw(fnames, sfreq=sfreq, ch_ind=ch_ind,
-                                stim_ind=stim_ind,
-                                replace_ch_names=replace_ch_names, verbose=verbose)
+    sfreq = SAMPLE_FREQS[board_name]
+    ch_ind = CHANNEL_INDICES[board_name]
+    stim_ind = STIM_INDICES[board_name]
+
+    return load_csv_as_raw(fnames, sfreq, ch_ind, stim_ind, replace_ch_names, verbose)
+
 
 
 def plot_conditions(epochs, conditions=OrderedDict(), ci=97.5, n_boot=1000,
                     title='', palette=None, ylim=(-6, 6),
                     diff_waveform=(1, 2)):
     """Plot ERP conditions.
-
     Args:
         epochs (mne.epochs): EEG epochs
-
     Keyword Args:
         conditions (OrderedDict): dictionary that contains the names of the
             conditions to plot as keys, and the list of corresponding marker
             numbers as value. E.g.,
-
                 conditions = {'Non-target': [0, 1],
                                'Target': [2, 3, 4]}
-
         ci (float): confidence interval in range [0, 100]
         n_boot (int): number of bootstrap samples
         title (str): title of the figure
@@ -133,7 +104,6 @@ def plot_conditions(epochs, conditions=OrderedDict(), ci=97.5, n_boot=1000,
         diff_waveform (tuple or None): tuple of ints indicating which
             conditions to subtract for producing the difference waveform.
             If None, do not plot a difference waveform
-
     Returns:
         (matplotlib.figure.Figure): figure object
         (list of matplotlib.axes._subplots.AxesSubplot): list of axes
@@ -190,15 +160,12 @@ def plot_conditions(epochs, conditions=OrderedDict(), ci=97.5, n_boot=1000,
 def plot_highlight_regions(x, y, hue, hue_thresh=0, xlabel='', ylabel='',
                            legend_str=()):
     """Plot a line with highlighted regions based on additional value.
-
     Plot a line and highlight ranges of x for which an additional value
     is lower than a threshold. For example, the additional value might be
     pvalues, and the threshold might be 0.05.
-
     Args:
         x (array_like): x coordinates
         y (array_like): y values of same shape as `x`
-
     Keyword Args:
         hue (array_like): values to be plotted as hue based on `hue_thresh`.
             Must be of the same shape as `x` and `y`.
@@ -207,7 +174,6 @@ def plot_highlight_regions(x, y, hue, hue_thresh=0, xlabel='', ylabel='',
         xlabel (str): x-axis label
         ylabel (str): y-axis label
         legend_str (tuple): legend for the line and the highlighted regions
-
     Returns:
         (matplotlib.figure.Figure): figure object
         (list of matplotlib.axes._subplots.AxesSubplot): list of axes
