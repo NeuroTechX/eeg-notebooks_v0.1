36db9e001e503a875d03ffbfb443d2e9b6d059c5
John Griffiths <j.davidgriffiths@gmail.com>
2020-07-24 14:07:29 -0400

    updated conf file

--- doc/conf.py
+++ doc/conf.py
@@ -1,41 +1,147 @@
 # -*- coding: utf-8 -*-
-#
-# Configuration file for the Sphinx documentation builder.
-#
-# For a full list of documentation options, see:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import sys
+import os
+import re
+
+# If we are building locally, or the build on Read the Docs looks like a PR
+# build, prefer to use the version of the theme in this repo, not the installed
+# version of the theme.
+def is_development_build():
+    # PR builds have an interger version
+    re_version = re.compile(r'^[\d]+$')
+    if 'READTHEDOCS' in os.environ:
+        version = os.environ.get('READTHEDOCS_VERSION', '')
+        if re_version.match(version):
+            return True
+        return False
+    return True
+
+if is_development_build():
+    sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('./demo/'))
+
+import sphinx_rtd_theme
+from sphinx.locale import _
 
 
-# ----------------------------------------------------------------------------
+project = 'EEG Notebooks'
+slug = re.sub(r'\W+', '-', project.lower())
+version = '0.1.0'
+release = '0.1.0'
+author = u'John Griffiths, Jadin Tredup, NeuroTechX, & Contributors'
+copyright = author
+language = 'en'
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.httpdomain',
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+source_suffix = ['.rst', '.md']
+exclude_patterns = []
+locale_dirs = ['locale/']
+gettext_compact = False
+
+master_doc = 'index'
+suppress_warnings = ['image.nonlocal_uri']
+pygments_style = 'default'
+
+intersphinx_mapping = {
+    'rtd': ('https://docs.readthedocs.io/en/latest/', None),
+    'sphinx': ('http://www.sphinx-doc.org/en/stable/', None),
+}
+
+html_theme = 'sphinx_rtd_theme'
+#html_theme_options = {
+#    'logo_only': False,#True,
+#    'navigation_depth': 5,
+#}
+html_context = {}
+
+if not 'READTHEDOCS' in os.environ:
+    html_static_path = ['_static/']
+    html_js_files = ['debug.js']
+
+    # Add fake versions for local QA of the menu
+    html_context['test_versions'] = list(map(
+        lambda x: str(x / 10),
+        range(1, 100)
+    ))
+
+#html_logo = "demo/static/logo-wordmark-light.svg"
+#html_logo = "img/eeg-notebooks_logo.png"
+html_show_sourcelink = True
+
+htmlhelp_basename = slug
+
+
+latex_documents = [
+  ('index', '{0}.tex'.format(slug), project, author, 'manual'),
+]
+
+man_pages = [
+    ('index', slug, project, [author], 1)
+]
+
+texinfo_documents = [
+  ('index', slug, project, author, slug, project, 'Miscellaneous'),
+]
+
+
+# Extensions to theme docs
+def setup(app):
+    from sphinx.domains.python import PyField
+    from sphinx.util.docfields import Field
+
+    app.add_object_type(
+        'confval',
+        'confval',
+        objname='configuration value',
+        indextemplate='pair: %s; configuration value',
+        doc_field_types=[
+            PyField(
+                'type',
+                label=_('Type'),
+                has_arg=False,
+                names=('type',),
+                bodyrolename='class'
+            ),
+            Field(
+                'default',
+                label=_('Default'),
+                has_arg=False,
+                names=('default',),
+            ),
+        ]
+    )
+
+    
+    # JG_ADD
+    app.add_css_file('theme_override.css')
+
+
+
+
+
+# ------------------------------------------------
+# Sphinx gallery stuff
 
 import os
 from os.path import dirname as up
-
 from datetime import date
-
 import sphinx_gallery
-import sphinx_bootstrap_theme
+#    import sphinx_bootstrap_theme
+#import sphinx_rtd_theme
 from sphinx_gallery.sorting import FileNameSortKey, ExplicitOrder
 
-
-# -- Project information -----------------------------------------------------
-
-# Set project information
-project = 'eeg-notebooks'
-copyright = '2020-{}, NeuroTechX'.format(date.today().year)
-author = 'John Griffiths'
-
-# Get and set the current version number
-#from fooof import __version__
-#version = __version__
-#release = version
-version = '0.1'
-release = '0.1'
-
-# -- General configuration ---------------------------------------------------
-
 # Add any Sphinx extension module names here, as strings.
-extensions = [
+extensions += [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
@@ -50,7 +156,11 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path .
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -84,32 +194,26 @@ pygments_style = 'sphinx'
 # Settings for sphinx_copybutton
 copybutton_prompt_text = "$ "
 
-# -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.
-html_theme = 'bootstrap'
-
-# Set the theme path explicitly
-#   This isn't always needed, but is useful so bulding docs doesn't fail on
-#   operating systems which don't have bootstrap on theme path
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-
-# Theme options to customize the look and feel, which are theme-specific.
-"""
 html_theme_options = {
-    'navbar_sidebarrel': False,
-    'navbar_links': [
-        ("API", "api"),
-        ("FAQ", "faq"),
-        ("Glossary", "glossary"),
-        ("Motivations", "auto_motivations/index"),
-        ("Tutorials", "auto_tutorials/index"),
-        ("Examples", "auto_examples/index"),
-        ("Reference", "reference"),
-        ("GitHub", "https://github.com/fooof-tools/fooof", True),
-    ],
-"""
+                      'canonical_url': '',
+                      'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+                      'logo_only': False,
+                      'display_version': True,
+                      'prev_next_buttons_location': 'bottom',
+                      'style_external_links': False,
+                      'vcs_pageview_mode': '',
+                      'style_nav_header_background': 'white',
+                      # Toc options
+                      'collapse_navigation': False,#True,
+                      'sticky_navigation': True,
+                      'navigation_depth': 4,
+                      'includehidden': True,
+                      'titles_only': False
+                                           }
 
+
+"""
 html_theme_options = {
     'navbar_sidebarrel': False,
     'navbar_links': [
@@ -133,42 +237,37 @@ html_theme_options = {
 # Settings for whether to copy over and show link rst source pages
 html_copy_source = False
 html_show_sourcelink = False
+"""
+
+
+# -- Extension configuration -------------------------------------------------
+
+
+# Theme options to customize the look and feel, which are theme-specific.
+
+# Settings for whether to copy over and show link rst source pages
+html_copy_source = False
+html_show_sourcelink = False
 
 
 # -- Extension configuration -------------------------------------------------
 
 # Configurations for sphinx gallery
+
+sphinx_gallery_conf = {'filename_pattern': '(?=.*r__)(?=.*.py)', 
+                       'examples_dirs': ['../examples','../examples/visual_n170', '../examples/visual_p300', '../examples/visual_cueing'],
+                       'gallery_dirs': ['auto_examples','auto_examples/visual_n170', 'auto_examples/visual_p300', 'auto_examples/visual_cueing'],
+                       'within_subsection_order': FileNameSortKey,
+                       'default_thumb_file': 'img/eeg-notebooks_logo.png',
+                       'backreferences_dir': 'generated',   # Where to drop linking files between examples & API
+                       'doc_module': ('eeg-notebooks'),
+                       'reference_url': {'eeg-notebooks': None},
+                       'remove_conffig_comments': True}
+
 """
 sphinx_gallery_conf = {
-    'examples_dirs': ['../examples', '../tutorials', '../motivations'],
-    'gallery_dirs': ['auto_examples', 'auto_tutorials', 'auto_motivations'],
-    'subsection_order' : ExplicitOrder(['../examples/manage',
-                                        '../examples/plots',
-                                        '../examples/sims',
-                                        '../examples/analyses',
-                                        '../examples/analyze_data',
-                                        '../motivations/concepts',
-                                        '../motivations/measurements',
-                                        '../examples/equipment_and_setup',
-                                        '../examples/making_recordings',
-                                        '../examples/stimulus_presentation',
-                                        '../examples/analyzing_experimental_results',
-                                        '../examples/complete_examples']),
-    'within_subsection_order': FileNameSortKey,
-    'default_thumb_file': 'img/eeg-notebooks_logo.png',
-    'backreferences_dir': 'generated',   # Where to drop linking files between examples & API
-    'doc_module': ('fooof',),
-    'reference_url': {'fooof': None},
-    'remove_config_comments': True,
-}
-""";
-
-
-#        'filename_pattern': '.py',
-
-sphinx_gallery_conf = {
         'filename_pattern': '.py', 
-        'examples_dirs': ['../examples','../background'],
+        'examples_dirs': ['../examples'],
         'gallery_dirs': ['auto_examples'],
         'subsection_order': ExplicitOrder([ '../examples/visual_n170',
                                             '../examples/visual_p300', 
@@ -187,4 +286,4 @@ sphinx_gallery_conf = {
     'remove_conffig_comments': True,
 }
 
-
+"""
