5b9c522f4aa58f598f5bc67bd3e2f52f88274fad
JadinTredup <Jadin.Tredup@gmail.com>
2020-07-31 13:04:03 -0700

    Fixed the file name generating function for the new standardized format to be used.

--- eegnb/__init__.py
+++ eegnb/__init__.py
@@ -8,8 +8,8 @@ def generate_save_fn(board_name, experiment, subject_id, session_nb, data_dir=DA
     '''Generates a file name with the proper trial number for the current subject/experiment combo'''
 
     # convert subject ID to 4-digit number
-    subject_str = '%04.f' % subject_id
-    session_str = 'session_%03.f' % session_nb
+    subject_str = 'subject%04.f' % subject_id
+    session_str = 'session%03.f' % session_nb
 
     # folder structure is /DATA_DIR/experiment/site/subject/session/*.csv
     recording_dir = path.join(data_dir, experiment, 'local', board_name, subject_str, session_str)
